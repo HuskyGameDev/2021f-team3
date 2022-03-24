dc15282dd3e6498e1ebb719ea4889e6cfd29c785
Caitlin O'Reilly <cloreill@mtu.edu>
2022-03-24 18:49:52 -0400

    bug fixing

--- Run to the Rescue/Assets/Scripts/PlayerDeath.cs
+++ Run to the Rescue/Assets/Scripts/PlayerDeath.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    public float invincibilityLength;
+    private float invincibilityCounter;
+  
+    void Update()
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (invincibilityCounter > 0)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            invincibilityCounter -= Time.deltaTime;
         }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (invincibilityCounter <= 0)
+        { if (collision.gameObject.CompareTag("Enemy"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            if (collision.gameObject.CompareTag("invincible"))
+            {
+                invincibilityCounter = invincibilityLength;
+            }
+
+
+        }
+
+
     }
 }
